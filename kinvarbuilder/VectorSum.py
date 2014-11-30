9003b95e8a46a2bbc3a3bf6a6bf12c87274621e6
Andre Holzner <andre.holzner@gmail.com>
2014-11-30 19:20:09 +0100

    added copyright / license headers

--- kinvarbuilder/VectorSum.py
+++ kinvarbuilder/VectorSum.py
@@ -1,5 +1,22 @@
 #!/usr/bin/env python
 
+# kinvarbuilder - A library for searching kinematic variables in a systematic way
+#
+# Copyright 2014 University of California, San Diego
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
 from .kinvarbuilder import CachingFunction, IllegalArgumentTypes
 
 from FourVector import FourVector
@@ -78,4 +95,3 @@ class VectorSum:
         # not exactly what the __repr__ function should return
         # but helps in debugging
         return self.__str__()
-

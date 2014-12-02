ed3df225ddfc000f74c524d24fd0e94fa5ee1106
Andre Holzner <andre.holzner@gmail.com>
2014-12-02 19:43:09 +0100

    factored out ROOT specific code from method TreeProcessor.makeTree(..) to a new class _RootTreeWriter

--- kinvarbuilder/TreeProcessor.py
+++ kinvarbuilder/TreeProcessor.py
@@ -19,6 +19,70 @@
 
 from .TreeReader import TreeReader
 
+#----------------------------------------------------------------------
+
+
+class _RootTreeWriter:
+    """
+    helper class for TreeProcessor for producing a ROOT tree
+    """
+
+    def __init__(self,outputFileName, outputTreeName):
+        #----------
+        # create an output tree with the variables
+        #----------
+
+        if outputFileName != None:
+            import ROOT
+            self.fout = ROOT.TFile(outputFileName, "RECREATE")
+        else:
+            self.fout = None
+
+        self.outputTreeName = outputTreeName
+
+
+    def setNumOutputEvents(self, numOutputEvents):
+        # not needed for ROOT files
+        pass
+
+    def setVariableNames(self, outputVarNames):
+        import ROOT
+        self.outTree = ROOT.TNtuple(self.outputTreeName,"output tree",
+                               ":".join(outputVarNames))
+
+        import array
+        # buffer for TTree.Fill(..)
+        self.outputValues =  array.array("f", [ 0.0 ] * len(outputVarNames))
+
+    def addEvent(self, values):
+        assert(len(values) == len(self.outputValues))
+
+        # copy this to the array for filling it into the ntuple
+        for index, value in enumerate(values):
+            self.outputValues[index] = value
+
+        # fill the output tree
+        self.outTree.Fill(self.outputValues)
+
+    def finish(self):
+        # write the output tree to the file
+        if self.fout != None:
+
+            import ROOT
+
+            self.fout.cd()
+            self.outTree.Write()
+
+            # TODO: go back to the ROOT directory we were before instead of going to ROOT.gROOT
+
+            ROOT.gROOT.cd()
+            self.fout.Close()
+
+
+
+
+#----------------------------------------------------------------------
+
 class TreeProcessor:
     """ reads an input tree and produces the additional output variables.
         Can be called multiple times after instantiation
@@ -53,8 +117,7 @@ class TreeProcessor:
 
     #----------------------------------------
 
-    def makeTree(self, inputTree, outputTreeName, outputFileName = None, maxEvents = None,
-                 # firstEvent = None,
+    def makeTree(self, inputTree, outputTreeName, outputFileName = None, firstEvent = 0, maxEvents = None,
                  progressCallback = None):
         """
         produce a ROOT output tree
@@ -64,7 +127,7 @@ class TreeProcessor:
         :param outputFileName: optional: if given, a TFile is created and the generated tree is written
             to this file
         :param maxEvents: process at most this number of events (unless it is None)
-        :param firstEvent: the number of the first event to process
+        :param: firstEvent is the index of the first event to process (zero based)
         :param progressCallback: a function taking the pointer to this class and the index (0 based) of
          the event about to be processed
         :return:
@@ -72,7 +135,24 @@ class TreeProcessor:
 
         treeReader = TreeReader(inputTree)
 
+        outputMaker = _RootTreeWriter(outputFileName, outputTreeName)
+
+        #----------
+        # determine the number of rows in the array to return
+        #----------
+        if maxEvents != None:
+            endEvent = min(firstEvent + maxEvents, treeReader.numEvents)
+        else:
+            endEvent = treeReader.numEvents
+
+        numEventsToProcess = endEvent - firstEvent
+
+        outputMaker.setNumOutputEvents(numEventsToProcess)
+
+        #----------
         # set the input tree
+        #----------
+
         # (note that for the moment this is not thread safe)
         for vector in self.varBuilder.inputVectors:
             vector.setTreeReader(treeReader)
@@ -80,24 +160,9 @@ class TreeProcessor:
         # add spectator expressions to the treeReader
         spectatorBuffers = [ treeReader.getVar(expression) for expression in self.spectatorExpressions ]
 
-        #----------
-        # create an output tree with the variables
-        #----------
-
-        if outputFileName != None:
-            import ROOT
-            fout = ROOT.TFile(outputFileName, "RECREATE")
-        else:
-            fout = None
-
         allOutputVarNames = self.varBuilder.outputVarnames + self.spectatorOutputVariableNames
 
-        outTree = ROOT.TNtuple(outputTreeName,"output tree",
-                               ":".join(allOutputVarNames))
-
-        import array
-        # buffer for TTree.Fill(..)
-        outputValues =  array.array("f", [ 0.0 ] * len(allOutputVarNames))
+        outputMaker.setVariableNames(allOutputVarNames)
 
         #----------
         # loop over all lines of the data given
@@ -109,7 +174,7 @@ class TreeProcessor:
                 break
 
             if progressCallback != None:
-                progressCallback(maxEvents, eventIndex)
+                progressCallback(numEventsToProcess, eventIndex)
 
             # read the event into memory
             treeReader.getEvent(eventIndex)
@@ -118,27 +183,20 @@ class TreeProcessor:
             for obj in self.varBuilder.outputScalars:
                 obj.newEvent()
 
-            # calculate the derived quantities
-            for index, derivedQuantity in enumerate(self.varBuilder.outputScalars):
-                outputValues[index] = derivedQuantity.getValue()
+            # add the quantities to the output object
+            outputMaker.addEvent(
+                # the fourvector based quantities
+                [ derivedQuantity.getValue() for derivedQuantity in self.varBuilder.outputScalars ] +
 
-            # copy the values for additional expressions to be put into the tree
-            for index, buffer in enumerate(spectatorBuffers):
-                outputValues[len(self.varBuilder.outputScalars) + index] = buffer[0]
+                # the spectator functions
+                [ buffer[0] for buffer in spectatorBuffers ]
+
+            )
 
             # TODO: add support for quantity not existing
-             # fill the output tree
-            outTree.Fill(outputValues)
 
-        # write the output tree to the file
-        if fout != None:
-            fout.cd()
-            outTree.Write()
 
-            # TODO: go back to the ROOT directory we were before instead of going to ROOT.gROOT
-
-            ROOT.gROOT.cd()
-            fout.Close()
+        outputMaker.finish()
 
     #----------------------------------------    
 

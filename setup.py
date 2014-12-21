be0c101a9adc7f6d62dc4335b4bae600f35e3670
Andre Holzner <andre.holzner@gmail.com>
2014-12-21 22:29:49 +0100

    initial commit of setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name = 'kinvarbuilder',
+    version = '0.0',
+    url = 'https://github.com/kinvarbuilder/kinvarbuilder/',
+    license = 'Apache Software License Version 2.0',
+    author = 'Andre Holzner',
+    tests_require = [],
+    install_requires = [],
+    author_email = 'andre.holzner@gmail.com',
+    description = 'A library for building kinematic variables systematically',
+    long_description = '',
+    packages = ['kinvarbuilder'],
+    include_package_data = True,
+    platforms = 'any',
+    classifiers = [
+        'Programming Language :: Python',
+        'Natural Language :: English',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        ],
+    extras_require = { }
+)
+

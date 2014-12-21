kinvarbuilder/kinvarbuilder

commit 14fa7448a54eddc5d6f6ece4fe0e19bbd7b83ed9

added protection against vectors which don't have an Eta(..) property (e.g. transverse vectors)

kinvarbuilder/functions/DeltaR.py
@@ -18,6 +18,7 @@
 
 
 from .VectorDifferenceQuantity import VectorDifferenceQuantity
+from ..kinvarbuilder import IllegalArgumentTypes
 
 import math
 
@@ -25,6 +26,9 @@ class DeltaR(VectorDifferenceQuantity):
     """distance in (eta,phi) plane between vectors """
 
     def __init__(self, vector1, vector2):
+
+        if not hasattr(vector1, 'Eta') or not hasattr(vector2,'Eta'):
+            raise IllegalArgumentTypes()
 
         # no requirement on the input vectors to be
         # fourvectors

commit 601ade5369bbf52785bb553889bbdc6069d28f86

fix for bug introduced in 14fa744: now checking whether the given vector sums are Fourvectors instead of insisting that the given objects have an Eta(..) attribute

kinvarbuilder/functions/DeltaR.py
@@ -27,7 +27,7 @@ class DeltaR(VectorDifferenceQuantity):
 
     def __init__(self, vector1, vector2):
 
-        if not hasattr(vector1, 'Eta') or not hasattr(vector2,'Eta'):
+        if not vector1.isFourVector() or not vector2.isFourVector():
             raise IllegalArgumentTypes()
 
         # no requirement on the input vectors to be